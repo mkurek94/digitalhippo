103836522af5bf482496762737389c22acd33e16
Mateusz Kurek <m.kurek1994@gmail.com>
2024-10-16 21:54:45 +0200

    Add close navbar handlers

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,17 +1,36 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import { NavItem } from "./NavItem";
 import { PRODUCT_CATEGORIES } from "@/config";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 export const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if(e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    }
+
+    document.addEventListener('keydown', handler);
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [setActiveIndex])
+
   const isAnyOpen = useMemo(() => activeIndex !== null, [activeIndex]);
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const handleOpen = () => {
           if (activeIndex === index) {
